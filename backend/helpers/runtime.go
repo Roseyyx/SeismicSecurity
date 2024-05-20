ad689a858a947d06b2592e469d6a27863aff9b70
Roseyyx <sverrevanhoepen12@outlook.com>
2024-05-20 23:15:55 +0200

    docker check fix

--- backend/helpers/runtime.go
+++ backend/helpers/runtime.go
@@ -4,16 +4,27 @@ import (
 	"log"
 	"main/backend/models"
 	utilities "main/backend/utils"
+	"os"
 )
 
 var UsingFile bool = false
 
 func Setup() {
+	if CheckDocker() && CheckIfDatabaseExists() {
+		log.Println("Using Docker")
+	}
 	if !CheckIfDatabaseExists() {
 		// TODO: Add a prompt to create a master password
 		UsingFile = true
 		CreateMasterPassword("password")
 	}
+}
+
+func CheckDocker() bool {
+	if _, err := os.Stat("/.dockerenv"); err == nil {
+		return true
+	}
+	return false
 }
 
 func CheckIfDatabaseExists() bool {
